bb7b479f51f8f3be79cceaf8aa5943bd1ad9735d
vivek-wo <vivek.wei.wu@gmail.com>
2021-04-28 19:51:37 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/vivek/wo/flutter/flutter_collection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vivek.wo.flutter.flutter_collection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/route/route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RouteMain());
+}
+
+class RouteMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Route Navigator Demo',
+    );
+  }
+}
